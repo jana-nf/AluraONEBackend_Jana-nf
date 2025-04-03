872538a83ea6d5045f1567939179836ea2ba25c1
Janaina Nascimento Feitoza <janaina.feitoza@aluno.ipog.edu.br>
2025-04-03 12:01:02 -0300

    Primeiras ideias de classes em JAVA

---
+++ src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil{
+    public static void main(String[] args){
+        String nome = "Jana";//Substitua "Jana" pelo seu nome
+        System.out.println("Olá,"+nome+"!");
+        System.out.println("Tudo bem?");
+    }
+}
---
+++ src/Subtracao.java
@@ -0,0 +1,6 @@
+public class Subtracao{
+    public static void main(String[] args){
+        int resultado = 10 - 5;
+        System.out.println(resultado);
+    }
+}
